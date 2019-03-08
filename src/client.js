bbd9acae557a87bcdf0ab64c0eb0a27bfb2e6f5c
Simon Auner <simonauner@gmail.com>
2019-03-08 23:05:43 +0100

    move firebase conf outside of src files

--- src/client.js
+++ src/client.js
@@ -2,23 +2,18 @@ import React from 'preact-compat';
 import ReactDom from 'preact-compat';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'preact-redux';
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/app';
 
 import Layout from './components/layout/layout.component';
 import createStore from './store';
 
+import firebaseConfig from '../firebase.config';
+
 const store = createStore();
 
 // Initialize Firebase
-var config = {
-    apiKey: 'AIzaSyDHvAJdBJNi21vZcl5yf7wnJHanRw0iPc4',
-    authDomain: 'tiny-chat-e8708.firebaseapp.com',
-    databaseURL: 'https://tiny-chat-e8708.firebaseio.com',
-    projectId: 'tiny-chat-e8708',
-    storageBucket: 'tiny-chat-e8708.appspot.com',
-    messagingSenderId: '281909622275',
-};
-initializeApp(config);
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDom.render(
     <Provider store={store}>
