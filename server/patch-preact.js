57c86df725a3d5109ebcd74e907d4ddbf2e1fc0a
Simon Auner <simonauner@gmail.com>
2019-03-09 00:29:48 +0100

    remove unused script

--- server/patch-preact.js
+++
@@ -1,6 +0,0 @@
-var moduleAlias = require('module-alias');
-
-moduleAlias.addAliases({
-    react: 'preact-compat/dist/preact-compat.min',
-    'react-dom': 'preact-compat/dist/preact-compat.min',
-});
