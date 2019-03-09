5ff743418f72135cc6e008d1c347ab4b02453a1e
Simon Auner <simonauner@gmail.com>
2019-03-09 11:32:58 +0100

    add css hack to get viewport 100 on mobile devices

--- src/client.js
+++ src/client.js
@@ -12,8 +12,17 @@ import firebaseConfig from '../firebase.config';
 const store = createStore();
 
 // Initialize Firebase
-
 firebase.initializeApp(firebaseConfig);
+
+// --------------------------------------------
+// Hack to get viewport 100% on mobile devices
+// thanks to https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+// --------------------------------------------
 
 ReactDom.render(
     <Provider store={store}>
